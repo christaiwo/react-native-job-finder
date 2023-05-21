8432ef31e42f50557426af112bf171dbd9b64ecb
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-21 08:56:06 +0100

    added the assets and other files

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+
+
+import { COLORS, icons, images,  SIZES} from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
+
+const Home = () => {
+    return (  
+        <View>
+            <Text>Home This</Text>
+        </View>
+    );
+}
+ 
+export default Home;
