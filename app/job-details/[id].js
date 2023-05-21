2d3542b9a61bb12df992d3c077c6f5f81a60fb16
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-21 16:32:29 +0100

    update specifies component

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -20,6 +20,24 @@ const JobDetails = () => {
     const onRefresh = () => {
 
     }
+
+
+    const displayTabContent = () => {
+        switch (activeTab) {
+            case 'Qualifications':  
+                return <Specifics
+                    title='Qualifications'
+                    points={data[0].job_highlights?.Qualifications ?? ['N/A']}
+                />
+            case 'About':
+                return <JobAbout 
+                    info={data[0].job_description ?? "No data provided"}
+                />
+            case 'Responsibilities': 
+            default:
+                break;
+        }
+    }
   return (
     <SafeAreaView style={{ flex:1, backgroundColor: COLORS.lightWhite }}>
         <Stack.Screen
@@ -63,7 +81,8 @@ const JobDetails = () => {
             />
 
 
-            
+            {displayTabContent()}
+
           </View>
         )}
       </ScrollView>
